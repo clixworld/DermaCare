353312a614d5de06b69065026d95bba86986285a
Cliden Olivares <cliden.tria@icloud.com>
2024-02-18 05:42:20 -0600

    Did a lot lol

--- gptresponse.py
+++ gptresponse.py
@@ -10,8 +10,8 @@ class ChatGPT():
         completion = openai.chat.completions.create(
             model = "gpt-3.5-turbo",
             messages = [
-                {"role": "system", "content": "You are a helpful assistant returning a general treatment option based on the type of acne and user skin type. Return a JSON with these keys: diagnosis, suggestion1, suggestion2, suggestion3, link. And give the values you think would fit best."},
-                {"role": "user", "content": f"Acne {user_acne}, Skin Type {skin_type}."}
+                {"role": "system", "content": "You are a helpful assistant returning a general treatment option based on the type of acne and user skin type. If acne is birthmark just wrtie positive affirmation that its natural and by birth. Also when returning link, don't use healthline. Return a JSON with these keys: diagnosis, overview, suggestion. For diagnosis, just return the type of acne given. For overview, return a summary about 3 to 5 sentences about the type of acne. For suggestion, just recommend ingredients based on type of acne and skin type along with suggested routine."},
+                {"role": "user", "content": f"{user_acne}, skin-type = {skin_type}."}
             ]
         )
         return(completion.choices[0].message.content)
--- server.py
+++ server.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request
 from gptresponse import ChatGPT
 from inference_sdk import InferenceHTTPClient
 import os
+import ast
 
 ai_bot = ChatGPT()
 
 app = Flask(__name__)
 
-# Initialize the InferenceHTTPClient with your API key and URL
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
-    api_key="w6Up93MdGNyb5HbWu18r"  # Replace with your actual API key
+    api_key="w6Up93MdGNyb5HbWu18r"
 )
 
 @app.route("/", methods=['POST', 'GET'])
@@ -24,12 +24,11 @@ def user_photos():
     all_classes = []
     
     for file in user_files:
-        # Save file to a temporary path
         temp_path = os.path.join('static/images', file.filename)
         file.save(temp_path)
         
         # Call the Roboflow API with the image
-        result = CLIENT.infer(temp_path, model_id="pimples-detection/4")
+        result = CLIENT.infer(temp_path, model_id="acne-detection-5x7wb/1")
         
         # Extract the class from the result
         class_name = result['predictions'][0]['class'] if result['predictions'] else None
@@ -40,14 +39,18 @@ def user_photos():
 
     # Pass the extracted classes to the chatbot
     bot_responses = []
+    exists = []
     for class_name in all_classes:
         if class_name is not None:
-            bot_response = ai_bot.messages(class_name, skin_type)
-            print(bot_response[0])
-            bot_responses.append(bot_response)
+            if class_name.upper() not in exists:
+                bot_response = ai_bot.messages(class_name, skin_type)
+                bot_response = ast.literal_eval(bot_response)
+                print(bot_response["diagnosis"])
+                bot_responses.append(bot_response)
+                exists.append(class_name.upper())
             
 
-    return render_template("return.html", bot_responses=bot_responses)
+    return render_template("return.html", bot_responses=bot_responses, skin_type=skin_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
